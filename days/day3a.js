9cc2769add64838bd3f59307a9223af245f3dc66
Leszek <leslawkula@gmail.com>
2022-12-03 11:47:15 +0100

    COA-2022 [feat]: day3

---
+++ days/day3a.js
@@ -0,0 +1,58 @@
+import { removeFromArray, getFirstHalfOfArray, getSecondHalfOfArray, getDuplicates } from '../tools/arrayTools.js';
+import { getArrayFromFileLines } from '../tools/fileTools.js';
+import { EMPTY_STRING, SMALL_A_ASCII_CODE, LARGE_A_ASCII_CODE } from '../tools/consts.js';
+import fs from "fs";
+
+const PATH_TO_INPUT_FILE = "../inputs/day3input.txt";
+
+const SMALL_ASCII_LETERS_SUBTRACTION = SMALL_A_ASCII_CODE - 1;
+const LARGE_ASCII_LETERS_SUBTRACTION = LARGE_A_ASCII_CODE - 1;
+const ADDITIONAL_POINTS_FOR_LETTER_SIZE = 26;
+
+export const getPointsForPrio = (prio) => {
+	const prioAsciiCode = prio.charCodeAt(0);
+
+	if (prioAsciiCode > SMALL_ASCII_LETERS_SUBTRACTION) {
+		return prioAsciiCode - SMALL_ASCII_LETERS_SUBTRACTION;
+	} else {
+		return prioAsciiCode - LARGE_ASCII_LETERS_SUBTRACTION + ADDITIONAL_POINTS_FOR_LETTER_SIZE;
+	}
+}
+
+const getRucksackPrio = (data) => {
+	const firstCompartment = getFirstHalfOfArray(data);
+	const secondCompartment = getSecondHalfOfArray(data);
+	const filteredArray = getDuplicates(firstCompartment, secondCompartment);
+
+	return filteredArray[0];
+}
+
+const gatRucksackPoints = (rucksackItems) => {
+	const prio = getRucksackPrio(Array.from(rucksackItems));
+
+	return getPointsForPrio(prio);
+}
+
+const calculatePoints = (rucksackArray) => {
+	let cleanRucksackArray = removeFromArray(EMPTY_STRING, rucksackArray);
+	let result = 0;
+
+	cleanRucksackArray.forEach((data) => {
+		result += gatRucksackPoints(data);
+	});
+
+	return result;
+}
+
+const solveDayThree = (inpuData) => {
+	const splitedInputData = getArrayFromFileLines(inpuData);
+	const result = calculatePoints(splitedInputData);
+
+	return result;
+}
+
+fs.readFile(PATH_TO_INPUT_FILE, 'utf-8', (err, data) => {
+	if (err) throw err;
+
+	console.log(solveDayThree(data));
+})
---
+++ days/day3b.js
@@ -0,0 +1,57 @@
+import { removeFromArray, getDuplicates } from '../tools/arrayTools.js';
+import { getArrayFromFileLines } from '../tools/fileTools.js';
+import { EMPTY_STRING, SMALL_A_ASCII_CODE, LARGE_A_ASCII_CODE } from '../tools/consts.js';
+import fs from "fs";
+
+const PATH_TO_INPUT_FILE = "../inputs/day3input.txt";
+const ELFS_IN_GROUP = 3;
+
+const SMALL_ASCII_LETERS_SUBTRACTION = SMALL_A_ASCII_CODE - 1;
+const LARGE_ASCII_LETERS_SUBTRACTION = LARGE_A_ASCII_CODE - 1;
+const ADDITIONAL_POINTS_FOR_LETTER_SIZE = 26;
+
+export const getPointsForPrio = (prio) => {
+	const prioAsciiCode = prio.charCodeAt(0);
+
+	if (prioAsciiCode > SMALL_ASCII_LETERS_SUBTRACTION) {
+		return prioAsciiCode - SMALL_ASCII_LETERS_SUBTRACTION;
+	} else {
+		return prioAsciiCode - LARGE_ASCII_LETERS_SUBTRACTION + ADDITIONAL_POINTS_FOR_LETTER_SIZE;
+	}
+}
+
+const getGroupBagdePrio = (elf1, elf2, elf3) => {
+	const filteredArray = getDuplicates(getDuplicates(elf1, elf2), elf3);
+
+	return filteredArray[0];
+}
+
+const gatPointsForAddingBadges = (elf1, elf2, elf3) => {
+	const prio = getGroupBagdePrio(Array.from(elf1), Array.from(elf2), Array.from(elf3));
+
+	return getPointsForPrio(prio);
+}
+
+const calculatePoints = (rucksackArray) => {
+	let cleanRucksackArray = removeFromArray(EMPTY_STRING, rucksackArray);
+	let result = 0;
+
+	for (let i = 0; i < cleanRucksackArray.length - 1; i = i + ELFS_IN_GROUP) {
+		result += gatPointsForAddingBadges(cleanRucksackArray[i], cleanRucksackArray[i + 1], cleanRucksackArray[i + 2]);
+	}
+
+	return result;
+}
+
+const solveDayThree = (inpuData) => {
+	const splitedInputData = getArrayFromFileLines(inpuData);
+	const result = calculatePoints(splitedInputData);
+
+	return result;
+}
+
+fs.readFile(PATH_TO_INPUT_FILE, 'utf-8', (err, data) => {
+	if (err) throw err;
+
+	console.log(solveDayThree(data));
+})
